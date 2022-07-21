953004c465da57eed98106c047109bde44e21095
Amine-kun <aminehlab@gmail.com>
2022-07-21 22:43:58 +0200

    some changes

--- public/js/home/isLoggedIn.js
+++ public/js/home/isLoggedIn.js
@@ -1,11 +1,12 @@
-import {userInfo} from '../user.js'
-
 const isSignedBtns = document.querySelector('.header-btns'),
       sidemenu = document.querySelector('.sideMenu'),
       sideMenuUser= document.querySelector('.userP');
 
 
-
+const userInfo = localStorage.getItem('user') !== 'undefined'
+                 ? JSON.parse (localStorage.getItem('user'))
+                  : localStorage.clear();
+                  
 //user isLoggedIn check
 const isLoggedIn=()=> {
     if (userInfo){ 
