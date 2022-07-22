2270e8ecf3d1adbc7853cf1638ba281af2beca51
Amine-kun <aminehlab@gmail.com>
2022-07-22 15:14:31 +0200

    setting env vars

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,14 @@
 
+  let key =  process.env.FIREBASE_APIKEY;
+  let id = process.env.APP_ID;
+
   let firebaseConfig = {
-    apiKey: process.env.FIREBASE_APIKEY,
+    apiKey: key,
     authDomain: "league-verse.firebaseapp.com",
     projectId: "league-verse",
     storageBucket: "league-verse.appspot.com",
     messagingSenderId: "894553556038",
-    appId: process.env.APP_ID
+    appId: id
   };
 
 
