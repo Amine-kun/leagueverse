e94c04209362824156e761de7f8442d12e08825a
Amine Berroukham <72416679+Amine-kun@users.noreply.github.com>
2023-07-25 18:51:26 +0100

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,12 +1,13 @@
 
-  let firebaseConfig = {
-    apiKey:'AIzaSyCWYl3gZhXwc9Dgfw-jQm2MTg77-zJDq7E',
-    authDomain: "league-verse.firebaseapp.com",
-    projectId: "league-verse",
-    storageBucket: "league-verse.appspot.com",
-    messagingSenderId: "894553556038",
-    appId: YOUR_APP_ID
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCWYl3gZhXwc9Dgfw-jQm2MTg77-zJDq7E",
+  authDomain: "league-verse.firebaseapp.com",
+  databaseURL: "https://league-verse-default-rtdb.firebaseio.com",
+  projectId: "league-verse",
+  storageBucket: "league-verse.appspot.com",
+  messagingSenderId: "894553556038",
+  appId: "1:894553556038:web:4faf1e5bed3ed9ecd97647"
+};
 
 
 firebase.initializeApp(firebaseConfig);
